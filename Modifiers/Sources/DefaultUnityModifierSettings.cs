ab8a1d75e98701048fc9e8a875227bd93f5e62d3
Oleg Dzhuraev <godlikeaurora@gmail.com>
2024-11-10 15:31:51 +0300

    Added: Support of manual Default settings initialization with your own data.

--- Modifiers/Sources/DefaultUnityModifierSettings.cs
+++ Modifiers/Sources/DefaultUnityModifierSettings.cs
@@ -12,10 +12,16 @@ namespace InsaneOne.Modifiers
 
 		public string[] SupportedMofifiers => supportedModifiers;
 
+		/// <summary> Call it on game initialize to manually set up your own default settings, otherwise it will try to load it from resources. </summary>
+		public static void Setup(DefaultUnityModifierSettings defaultSettings) => instance = defaultSettings;
+
+		/// <summary> Returns global default settings asset. You need to setup it manually, or it will try to load it from resources (if exist). </summary>
 		public static DefaultUnityModifierSettings Get()
 		{
 			if (!instance)
 			{
+				Debug.LogWarning($"No Default Unity Modifier Settings was setup by using {nameof(Setup)} method! Trying to find it in Resources...");
+
 				instance = Resources.Load<DefaultUnityModifierSettings>("DefaultModifierSettings");
 				
 				if (instance)
