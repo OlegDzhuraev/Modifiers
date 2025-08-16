c4d097a5e300a693d3b79c7a89a422b5576b6752
Oleg Dzhuraev <godlikeaurora@gmail.com>
2025-08-16 20:43:48 +0300

    [+] Browse button in CSV Tools
    [+] More license texts

--- Modifiers/Sources/Tools/Editor/UnityCsvWindow.cs
+++ Modifiers/Sources/Tools/Editor/UnityCsvWindow.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Oleg Dzhuraev <godlikeaurora@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #if UNITY_EDITOR
 using System;
 using System.IO;
@@ -66,6 +82,13 @@ namespace InsaneOne.Modifiers.Tools
 			{
 				GUILayout.Label("Import from path");
 				importPath = GUILayout.TextField(importPath);
+			}
+
+			if (GUILayout.Button("Browse..."))
+			{
+				var path = EditorUtility.OpenFilePanel("Select a file", Application.dataPath, "csv");
+				if (!string.IsNullOrEmpty(path))
+					importPath = path;
 			}
 			GUILayout.EndHorizontal();
 
